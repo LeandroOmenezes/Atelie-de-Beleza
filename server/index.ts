f01a279c246386ac482ebd6f8fe718ab13c5c69d
leandroOmenezes <lleandro.m32@gmail.com>
2026-08-17 19:40:48 -0300

    Fix CSP for font-awesome mobile menu

--- server/index.ts
+++ server/index.ts
@@ -12,10 +12,10 @@ app.use((req, res, next) => {
     "Content-Security-Policy",
     "default-src 'self'; " +
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sdk.mercadopago.com https://*.mercadopago.com https://*.mlstatic.com; " +
-      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.mercadopago.com; " +
+      "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://cdnjs.cloudflare.com https://*.mercadopago.com; " +
       "img-src 'self' data: blob: https://*.mercadopago.com https://*.mlstatic.com https://images.unsplash.com; " +
-      "connect-src 'self' https://api.mercadopago.com https://sdk.mercadopago.com https://*.mercadopago.com https://*.mlstatic.com wss: ws:; " +
-      "font-src 'self' data: https://fonts.gstatic.com; " +
+      "connect-src 'self' https://api.mercadopago.com https://sdk.mercadopago.com https://*.mercadopago.com https://*.mlstatic.com https://cdnjs.cloudflare.com wss: ws:; " +
+      "font-src 'self' data: https://fonts.gstatic.com https://cdnjs.cloudflare.com; " +
       "frame-src 'self' https://www.mercadopago.com https://*.mercadopago.com; " +
       "object-src 'none'; " +
       "base-uri 'self'; " +
